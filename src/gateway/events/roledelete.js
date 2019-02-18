e11f0811a8cdae41e465d96eead15608212dd8f0
Kevlar <36104385+KevvyCodes@users.noreply.github.com>
2019-02-18 20:37:06 +0800

    Fix the invalid guild problem

--- src/gateway/events/roledelete.js
+++ src/gateway/events/roledelete.js
@@ -13,6 +13,11 @@ class RoleDelete {
     if (!packet.d.guild_id) return shard.client.emit('error', new Error('ROLE DELETED BUT NO GUILD FOUND!'));
 
     let guild = shard.client.guilds.get(packet.d.guild_id);
+    
+    // If no guild was found, we should not emit this event since it means that the bot left the guild
+    if (!guild)
+      return;
+
     let role = guild.roles.get(packet.d.role_id);
 
     guild.roles.delete(role.id);
